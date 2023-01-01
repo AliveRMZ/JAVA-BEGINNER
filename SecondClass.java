fefd225cc9441fb640d9f793adaa0516e2d2e81b
AliveRMZ <121676930+AliveRMZ@users.noreply.github.com>
2023-01-01 12:25:34 +0530

    Add files via upload
    learned about variables and output

---
+++ SecondClass.java
@@ -0,0 +1,14 @@
+class SecondClass{
+        public static void main(String[] args) {
+            //OUTPUTS AND VARIABLES
+            String name = "Rameez Ahmad";
+            int age= 13;
+            double ntwr= 00.00;
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(ntwr);
+
+            
+        
+    }
+}
