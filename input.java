36c3fa9ec11d01488e1e287c6514786d78a2c8c6
AliveRMZ <121676930+AliveRMZ@users.noreply.github.com>
2023-01-01 12:26:50 +0530

    Add files via upload
    ques on input, variables and output

---
+++ input.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class input{
+    public static void main(String[] args) {
+        //INPUT OF RADIUS
+        Scanner sc = new Scanner (System.in);
+        //System.out.print("ENTER RADIUS : ");
+        //double r = sc.nextDouble();
+
+        //CALCULATIONS
+        //double cir= 2*3.14*r;
+        //double ar=3.14*r*r;
+
+        //OUTPUT OF CIRCUMFERENCE AND AREA
+        //System.out.println(cir);
+        //System.out.println(ar);
+
+        //TABLE 
+        System.out.print("ENTER A NUMBER : ");
+        int a = sc.nextInt();
+        System.out.println(1*a);
+        System.out.println(2*a);
+        System.out.println(3*a);
+        System.out.println(4*a);
+        System.out.println(5*a);
+        System.out.println(6*a);
+        System.out.println(7*a);
+        System.out.println(8*a);
+        System.out.println(9*a);
+        System.out.println(10*a);
+
+    }
+}
+    
+
